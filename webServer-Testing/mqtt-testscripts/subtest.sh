814f0e5a0b49a5e5707d41189b18d3827fcd2401
maanavkoladia <maanav.koladia@gmail.com>
2025-04-11 12:36:26 -0500

    upated subtest

--- webServer-Testing/mqtt-testscripts/subtest.sh
+++ webServer-Testing/mqtt-testscripts/subtest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 while true; do
-  # Publish a test message to the broker
-  mosquitto_pub -h broker.hivemq.com -t "/seniorDesign/c2s" -m "freaky mesg"
-  sleep 1  # Add a 5-second delay to avoid flooding the broker
+    # Publish a test message to the Mosquitto test broker
+    mosquitto_pub -h test.mosquitto.org -t "/seniorDesign/c2s" -m "1,3,5,7,9,11,30,100"
+    sleep 1 # Send one message per second
 done
-
